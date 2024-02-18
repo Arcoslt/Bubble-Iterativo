67d07de233056654bd2384a3136bd5d10809c38a
Marcos <marcos.teixeira2@ufu.br>
2024-02-18 20:03:45 -0300

    Update BubbleSort_iterative.py
    Adicionando comentários

--- BubbleSort_iterative.py
+++ BubbleSort_iterative.py
@@ -1,16 +1,17 @@
 from random import randint
 
 def main():
+    #Criação do vetor com tamanho desejado e com inteiros aleatórios
     tamanho = input('Tamanho do Vetor: ')
     tamanho = int(tamanho)
     vet = []
-
     for i in range(tamanho):
         vet.append(randint(1, tamanho))
 
+    #Bubble sort
     for i in range(tamanho-1):
         for j in range(tamanho-1-i):
-            #Troca pos do vet com a do proximo, caso o proximo seja menor que o atual
+            #Troca posição do vet com a do próximo, caso o proximo seja menor que o atual
             if vet[j]>vet[j+1]:
                 aux=vet[j+1]
                 vet[j+1]=vet[j]
